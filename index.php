d907d742946bf6cca1ac1d7c49a45156a9146f87
tayyabaziz <tayyabaziz5@gmail.com>
2020-02-11 17:27:31 +0500

    no message

--- index.php
+++ index.php
@@ -51,14 +51,11 @@ if (isset($_POST['post_submit'])) {
         }
 
         .theme-content {
-            position: fixed;
-            height: 70% !important;
+            position: relative;
+            height: 835px !important;
             margin: 0 100px;
             left: 0;
             right: 0;
-            top: 50%;
-            -webkit-transform: translateY(-50%);
-            transform: translateY(-50%);
         }
 
         .theme-content .icon {
@@ -71,153 +68,163 @@ if (isset($_POST['post_submit'])) {
         }
 
         .bottom-logo {
-            height: 60px;
+            height: 70px;
         }
 
         .title {
-            margin-top: 2.5rem !important;
             font-weight: 600;
         }
 
         .footer {
-            position: fixed;
             margin: 0 90px;
             left: 0;
             right: 0;
         }
 
         .main-img {
-            height: 300px;
+            width: 350px;
+        }
+
+        .description {
+            font-size: 20px;
+            line-height: 30px;
         }
 
         .congrats-text {
-            font-size: 20px;
+            font-size: 24px;
         }
 
         .marker {
             width: 10px;
+        }
+
+        .my-content {
+            width: 1920px !important;
+            height: 1080px !important;
         }
     </style>
 </head>
 
 <body class="w-100">
     <?php if (!isset($_POST['post_submit'])) { ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-8 offset-2">
-                <form autocomplete="off" class="mt-5" action="" method="POST" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    Name
-                                </div>
-                            </div>
-                            <input required name="fullname" class="form-control" placeholder="Name">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    Position
-                                </div>
-                            </div>
-                            <input required name="position" class="form-control" placeholder="Position">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    City
-                                </div>
-                            </div>
-                            <select required class="form-control" name="city">
-                                <option>Karachi</option>
-                                <option>Lahore</option>
-                                <option>Islamabad</option>
-                                <option>Faisalabad</option>
-                                <option>Rawalpindi</option>
-                                <option>Hyderabad</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    Gender
-                                </div>
-                            </div>
-                            <div class="form-control">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" id="maleGender" name="gender" value="him" required>
-                                    <label class="form-check-label" for="maleGender">Male</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" id="femaleGender" name="gender" value="her" required>
-                                    <label class="form-check-label" for="femaleGender">Female</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    Description
-                                </div>
-                            </div>
-                            <textarea class="form-control" name="description" required></textarea>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    Image
-                                </div>
-                            </div>
-                            <input type="file" class="form-control form-control-file" name="image" accept="image/*" required>
-                        </div>
-                    </div>
-                    <button type="submit" name="post_submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
-    <?php } else { ?>
-    <div class="heading">
-        <h1 class="title text-center"><?= $_POST['position'] ?? "" ?></h1>
-    </div>
-    <div class="bg-theme-primary theme-content rounded shadow">
-        <div class="inner-body my-5 mx-4">
+        <div class="container">
             <div class="row">
-                <div class="mx-2 text-center">
-                    <img class="main-img img-fluid" src="<?= $newFileName ?>" />
-                </div>
-                <div class="col text-white mr-4">
-                    <div>
-                        <span class="float-right my-2">Location: <?= $_POST['city'] ?? "" ?></span>
-                        <h4><?= $_POST['fullname'] ?? "" ?></h4>
-                    </div>
-                    <hr class="mb-2">
-                    <p><?= $_POST['position'] ?? "" ?></p>
-                    <p class="font-weight-bold text-dark"><?= $_POST['description'] ?? "" ?></p>
-                    <p class="font-weight-bold congrats-text">
-                        Please extend your support and congratulate <?= $_POST['gender'] ?? "" ?> in the new role.
-                    </p>
-
+                <div class="col-sm-8 offset-2">
+                    <form autocomplete="off" class="mt-5" action="" method="POST" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        Name
+                                    </div>
+                                </div>
+                                <input required name="fullname" class="form-control" placeholder="Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        Position
+                                    </div>
+                                </div>
+                                <input required name="position" class="form-control" placeholder="Position">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        City
+                                    </div>
+                                </div>
+                                <select required class="form-control" name="city">
+                                    <option>Karachi</option>
+                                    <option>Lahore</option>
+                                    <option>Islamabad</option>
+                                    <option>Faisalabad</option>
+                                    <option>Rawalpindi</option>
+                                    <option>Hyderabad</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        Gender
+                                    </div>
+                                </div>
+                                <div class="form-control">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" id="maleGender" name="gender" value="him" required>
+                                        <label class="form-check-label" for="maleGender">Male</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" id="femaleGender" name="gender" value="her" required>
+                                        <label class="form-check-label" for="femaleGender">Female</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        Description
+                                    </div>
+                                </div>
+                                <textarea class="form-control" name="description" required></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        Image
+                                    </div>
+                                </div>
+                                <input type="file" class="form-control form-control-file" name="image" accept="image/*" required>
+                            </div>
+                        </div>
+                        <button type="submit" name="post_submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
             </div>
-            <img class=" icon img-fluid" src="announcement-logo.svg" />
         </div>
-    </div>
-    <div class="fixed-bottom pb-5 footer">
-        <img class="img-fluid bottom-logo float-left" src="em_logo_or.png" alt="Eat Mubarak">
-        <img class="img-fluid bottom-logo float-right" src="hr-test-logo.png" alt="Eat Mubarak">
-    </div>
+    <?php } else { ?>
+        <div class="my-content m-auto">
+            <div class="heading">
+                <h1 class="title text-center py-4"><?= $_POST['position'] ?? "" ?></h1>
+            </div>
+            <div class="bg-theme-primary theme-content rounded shadow">
+                <div class="inner-body py-5 px-4">
+                    <div class="row">
+                        <div class="mx-2 text-center">
+                            <img class="main-img img-fluid" src="<?= $newFileName ?>" />
+                        </div>
+                        <div class="col text-white mr-4">
+                            <div>
+                                <span class="float-right my-2">Location: <?= $_POST['city'] ?? "" ?></span>
+                                <h3 class="font-weight-bold"><?= $_POST['fullname'] ?? "" ?></h3>
+                            </div>
+                            <hr class="mb-0 mt-2">
+                            <p><?= $_POST['position'] ?? "" ?></p>
+                            <p class="font-weight-bold description mt-4 mb-4"><?= $_POST['description'] ?? "" ?></p>
+                            <p class="font-weight-bolder congrats-text">
+                                Please extend your support and congratulate <?= $_POST['gender'] ?? "" ?> in the new role.
+                            </p>
+
+                        </div>
+                    </div>
+                    <img class=" icon img-fluid" src="announcement-logo.svg" />
+                </div>
+            </div>
+            <div class="pb-5 footer my-5">
+                <img class="img-fluid bottom-logo float-left" src="em_logo_or.png" alt="Eat Mubarak">
+                <img class="img-fluid bottom-logo float-right" src="hr-test-logo.png" alt="Eat Mubarak">
+            </div>
+        </div>
     <?php } ?>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -227,7 +234,7 @@ if (isset($_POST['post_submit'])) {
 
     <?php if (isset($_POST['post_submit'])) { ?>
         <script>
-            html2canvas(document.body).then(canvas => {
+            html2canvas(document.querySelector(".my-content")).then(canvas => {
                 var img = canvas.toDataURL("image/png");
                 var dlLink = document.createElement('a');
                 dlLink.download = "<?= $_POST['fullname'] . " - " . $_POST['position'] ?? "" ?>.png";
@@ -237,4 +244,5 @@ if (isset($_POST['post_submit'])) {
         </script>
     <?php } ?>
 </body>
+
 </html>
